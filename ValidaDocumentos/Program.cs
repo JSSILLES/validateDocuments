774cbdd3a9fdf9cbd8185a32032a498a34c316c4
Jacqueline Silva Silles <jacqueline@gomara.tech>
2021-05-05 19:37:09 -0300

    Validades CNPJ and CPF

---
+++ ValidaDocumentos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidaDocumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cpf = "37524919816";
+
+            ValidaDocumentoUtil valida = new ValidaDocumentoUtil();
+
+            var retorno = valida.ValidaCPF(cpf);
+
+            Console.WriteLine(retorno);
+
+            Console.ReadKey();
+
+        }
+    }
+}
